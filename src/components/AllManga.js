e46f7728dfaae622696fc90c875e5e4125e83a16
pakinporing <pakinporing@gmail.com>
2023-02-10 20:37:19 +0700

    rtrt

--- src/components/AllManga.js
+++ src/components/AllManga.js
@@ -1,20 +1,29 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import img from '../assets/Allmanga.png';
 
 export default function AllManga({ manga }) {
+  const navigate = useNavigate();
+
   console.log(manga);
   return (
     <>
-      {manga?.map((el) => {
-        return (
-          <div className="flex flex-col items-center p-[40px]">
-            <img src={img} />
-            <div className="w-[995px] h-[2126px] bg-white rounded-[30px] p-[40px] shadow-sm">
-              <p>{el.mangaName}</p>
-            </div>
-          </div>
-        );
-      })}
+      <div className="flex flex-col items-center p-[40px]">
+        <img src={img} />
+        <div className="w-[995px] h-[2126px] bg-white rounded-[30px] p-[40px] shadow-sm">
+          {manga?.map((el) => {
+            return (
+              <p
+                onClick={() => {
+                  navigate(`/mangapage/${el.id}`);
+                }}
+              >
+                {el.mangaName}
+              </p>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
--- src/components/Read.js
+++ src/components/Read.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export default function Read({ src }) {
+  const { mangaId } = useParams();
+  const navigate = useNavigate();
+  console.log(mangaId);
   return (
     <div>
       <div className="flex justify-center">
         <img src={src} />
       </div>
       <div className="flex justify-center gap-10 p-[30px]">
-        <button className="border-[2px] rounded-[30px] w-[187px] h-[57px] bg-[#D8D3CD] text-[#ffffff]">
+        {/* <button className="border-[2px] rounded-[30px] w-[187px] h-[57px] bg-[#D8D3CD] text-[#ffffff]">
           ก่อนหน้า
-        </button>
-        <button className="border-[2px] rounded-[30px] w-[187px] h-[57px] bg-[#86AED1] text-[#ffffff]">
+        </button> */}
+        <button
+          className="border-[2px] rounded-[30px] w-[187px] h-[57px] bg-[#86AED1] text-[#ffffff]"
+          onClick={() => {
+            navigate(`/mangapage/${mangaId}`);
+          }}
+        >
           หน้าหลัก
         </button>
-        <button className="border-[2px] rounded-[30px] w-[187px] h-[57px] bg-[#FFBC90] text-[#ffffff]">
+        {/* <button className="border-[2px] rounded-[30px] w-[187px] h-[57px] bg-[#FFBC90] text-[#ffffff]">
           ถัดไป
-        </button>
+        </button> */}
       </div>
     </div>
   );
